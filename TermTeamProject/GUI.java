d99705318a1b2ca6ff4d0b68a0b1d2974b730024
Saul Sanchez <sanchezs@moravian.edu>
2024-02-15 11:08:09 -0500

    Created folder for project containing gui,main, and teamplayer classes

---
+++ TermTeamProject/GUI.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JFrame {
+    
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 700;
+    private TeamPlayer roster;
+    private JPanel buttonPanel;
+
+
+    public GUI() {   
+        setTitle("Women's Basketball Roster");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+    }
+}
---
+++ TermTeamProject/TeamPlayer.java
@@ -0,0 +1,11 @@
+public class TeamPlayer {
+    private String name;
+    private String position;
+    private int number;
+    private int classYear;
+    private int height;
+    private int weight;
+    
+    
+}
+
