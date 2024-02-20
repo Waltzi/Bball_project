61107c84b603ac696543c0966b3db109b0d420e4
Saul Sanchez <sanchezs@moravian.edu>
2024-02-20 10:10:03 -0500

    docstrings added

--- TermTeamProject/TeamPlayer.java
+++ TermTeamProject/TeamPlayer.java
@@ -1,3 +1,10 @@
+/**
+ * TeamPlayer class is used to create TeamPlayer objects.
+ * Each TeamPlayer object has a studentID, firstName, lastName, position, number, classYear, height, and weight.
+ * The class contains a default constructor, a constructor with parameters, and getter and setter methods for each field.
+ * The class also contains a toString method to return the fields of the object as a string.
+ */
+
 public class TeamPlayer {
 
     private int studentID;
@@ -9,6 +16,10 @@ public class TeamPlayer {
     private int height;
     private int weight;
 
+    /**
+     * Default constructor for TeamPlayer class.
+     * Initializes all fields to default values.
+     */
     public TeamPlayer() {
         this.studentID = 0;
         this.firstName = "";
@@ -20,6 +31,18 @@ public class TeamPlayer {
         this.weight = 0;
     }
 
+    /**
+     * Constructor for TeamPlayer class.
+     * Initializes all fields to the values passed as parameters.
+     * @param studentID
+     * @param firstName
+     * @param lastName
+     * @param position
+     * @param number
+     * @param classYear
+     * @param height
+     * @param weight
+     */
     public TeamPlayer(int studentID, String firstName, String lastName, String position, int number, 
     int classYear, int height, int weight) {
         this.studentID = studentID;
@@ -32,67 +55,132 @@ public class TeamPlayer {
         this.weight = weight;
     }
 
+    /**
+     * Getter method for studentID field.
+     * @return studentID
+     */
     public int getStudentID() {
         return studentID;
     }
 
+    /**
+     * Getter method for firstName field.
+     * @return firstName
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Getter method for lastName field.
+     * @return lastName
+     */
     public String getLastName() {
         return lastName;
     }
     
+    /**
+     * Getter method for position field.
+     * @return position
+     */
     public String getPosition() {
         return position;
     }
 
+    /**
+     * Getter method for number field.
+     * @return number
+     */
     public int getNumber() {
         return number;
     }
 
+    /**
+     * Getter method for classYear field.
+     * @return classYear
+     */
     public int getClassYear() {
         return classYear;
     }
 
+    /**
+     * Getter method for height field.
+     * @return height
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * Getter method for weight field.
+     * @return weight
+     */
     public int getWeight() {
         return weight;
     }
 
+    /**
+     * Setter method for studentID field.
+     * @param studentID
+     */
     public void setStudentID(int studentID) {
         this.studentID = studentID;
     }
 
+    /**
+     * Setter method for firstName and lastName fields.
+     * @param firstName
+     * @param lastName
+     */
     public void setName(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    /**
+     * Setter method for position field.
+     * @param position
+     */
     public void setPosition(String position) {
         this.position = position;
     }
 
+    /**
+     * Setter method for number field.
+     * @param number
+     */
     public void setNumber(int number) {
         this.number = number;
     }
 
+    /**
+     * Setter method for classYear field.
+     * @param classYear
+     */
     public void setClassYear(int classYear) {
         this.classYear = classYear;
     }
 
+    /**
+     * Setter method for height field.
+     * @param height
+     */
     public void setHeight(int height) {
         this.height = height;
     }
 
+    /**
+     * Setter method for weight field.
+     * @param weight
+     */
     public void setWeight(int weight) {
         this.weight = weight;
     }
 
+    /**
+     * toString method to return the fields of the object as a string.
+     * @return studentID, firstName, lastName, position, number, classYear, height, weight
+     */
     @Override
     public String toString() {
         return  studentID +
