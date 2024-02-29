27fed014e6152284992ab1f2d02822079568e3c5
Saul Sanchez <sanchezs@moravian.edu>
2024-02-29 17:03:23 -0500

    created springboot project (not complete)

---
+++ FrontTerm/termproject/src/test/java/project/termproject/TermprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.termproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
