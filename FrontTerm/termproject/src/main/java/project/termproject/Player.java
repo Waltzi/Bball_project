81390bbf3212164585a2ada021e195a9a5db0364
Nathaniel Hajel <nhajel17@gmail.com>
2024-03-26 09:38:53 -0400

    Added getters and setters to the Player.java class

--- FrontTerm/termproject/src/main/java/project/termproject/Player.java
+++ FrontTerm/termproject/src/main/java/project/termproject/Player.java
@@ -15,5 +15,30 @@ public class Player {
     // Add other fields as needed
     
     // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    
     
 }
