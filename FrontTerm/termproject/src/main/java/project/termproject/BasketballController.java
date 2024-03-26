6a01d293bdf77755a85b3aac4c0d83e31cb27e71
Saul Sanchez <sanchezs@moravian.edu>
2024-03-26 10:39:19 -0400

    Deleted one main

--- FrontTerm/termproject/src/main/java/project/termproject/BasketballController.java
+++ FrontTerm/termproject/src/main/java/project/termproject/BasketballController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import project.termproject.PlayerRepository; // Add the missing import statement
 
@@ -25,7 +27,35 @@ public class BasketballController {
         return "index";
     }
 
-    // Add other controller methods as needed
-    
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("player", new Player());
+        return "create";
+    }
+
+    @PostMapping("/save")
+    public String save(Player player) {
+        playerRepository.save(player);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, Model model) {
+        model.addAttribute("player", playerRepository.findById(id).orElse(new Player()));
+        return "edit";
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(@PathVariable Long id, Player player) {
+        playerRepository.save(player);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        playerRepository.deleteById(id);
+        return "redirect:/";
+    }
+
 
 }
