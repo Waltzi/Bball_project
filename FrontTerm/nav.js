3b9c49e7a4cdc5df2cc94dcc1774c56a206772a9
Brandon Yang <brandon.yang1210@gmail.com>
2024-04-01 17:30:43 -0400

    created a whole new file for the nav bar that is in JS

---
+++ FrontTerm/nav.js
@@ -0,0 +1,10 @@
+function NavBar() {
+    return (
+        <nav className="NavBar">
+            <h1 className="left-align">Moravian Women's Basketball</h1>
+            <ul className="right-align">
+                {/* Add your navigation links here */}
+            </ul>
+        </nav>
+    );
+}
