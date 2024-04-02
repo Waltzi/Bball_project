0ecc5484c8f0c69c13dbd645385ba7c319bfb4e8
Brandon Yang <brandon.yang1210@gmail.com>
2024-04-02 09:37:57 -0400

    this file contains fetching for apis from springboot

---
+++ FrontTerm/app.js
@@ -0,0 +1,10 @@
+fetch('http://localhost:5500/api/data')
+  .then(response => response.json())
+  .then(data => {
+    // Handle the data received from the backend
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors that occur during the request
+    console.error('Error fetching data:', error);
+  });
