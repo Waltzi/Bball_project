b9e8c50cb551650a9fa3f2c5fe74eb54d97876b7
Saul Sanchez <sanchezs@moravian.edu>
2024-04-16 10:54:52 -0400

    Springboot finally working, yay

--- FrontTerm/termproject/src/main/java/project/termproject/BasketballController.java
+++ FrontTerm/termproject/src/main/java/project/termproject/BasketballController.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -22,8 +22,10 @@ public class BasketballController {
     @Autowired
     private ThreePointService threePointService;
 
-    public BasketballController(PlayerService playerService) {
+    public BasketballController(PlayerService playerService, FreeThrowService freeThrowService, ThreePointService threePointService) {
         this.playerService = playerService;
+        this.freeThrowService = freeThrowService;
+        this.threePointService = threePointService;
     }
 
     @GetMapping("/getPlayers")
