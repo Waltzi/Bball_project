255e8a14d20e264436ffa438d2777349531ab833
Brandon Yang <brandon.yang1210@gmail.com>
2024-04-26 10:18:20 -0400

    remvoed the JS for the freethrow

--- FrontTerm/app.js
+++ FrontTerm/app.js
@@ -37,50 +37,6 @@ async function editPlayer(player) {
   }
 }
 
-// Funtion to add a freethrow to the API
-async function addFreeThrow(freeThrow) {
-  try {
-      const response = await fetch('http://localhost:8080/basketball_api/addFreeThrow', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },  
-          body: JSON.stringify(freeThrow)
-      });
-      if (!response.ok) {
-          throw new Error('Failed to add free throw');
-      }
-      const addedFreeThrow = await response.json();
-      console.log('Added free throw:', addedFreeThrow);
-      return addedFreeThrow;
-  } catch (error) {
-      console.error('Error adding free throw:', error);
-      throw error;
-  }
-}
-
-// Function to edit a freethrow in the API
-async function editFreeThrow(freeThrow) {
-  try {
-      const response = await fetch('http://localhost:8080/basketball_api/editFreeThrow', {
-          method: 'PUT',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(freeThrow)
-      });
-      if (!response.ok) {
-          throw new Error('Failed to edit free throw');
-      }
-      const editedFreeThrow = await response.json();
-      console.log('Edited free throw:', editedFreeThrow);
-      return editedFreeThrow;
-  } catch (error) {
-      console.error('Error editing free throw:', error);
-      throw error;
-  }
-}
-
 // Function to add a 3-pointer to the API
 async function addThreePointer(threePointer) {
   try {
