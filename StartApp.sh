6a5f5677f8f649a8a703b40d0d3b52079dfb0031
Saul Sanchez <sanchezs@moravian.edu>
2024-04-28 19:32:12 -0400

    modified free throw and three pointer created script for app and added app zip file

---
+++ StartApp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$SCRIPT_DIR/FrontTerm/termproject/"
+./gradlew bootRun &
+open -a "Google Chrome" http://127.0.0.1:5500/FrontTerm/index.html
