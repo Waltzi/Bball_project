1f802b5d04559c32450598c55ea820ef725695e3
Saul Sanchez <sanchezs@moravian.edu>
2024-04-28 20:43:17 -0400

    added app dmg file for app again

--- StartApp.sh
+++ StartApp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR/FrontTerm/termproject/"
-chmod +x FrontTerm/termproject/gradlew
-./gradlew bootRun &
+chmod +x "$SCRIPT_DIR/FrontTerm/termproject/gradlew"
+"$SCRIPT_DIR/FrontTerm/termproject/gradlew" bootRun &
 open -a "Google Chrome" http://127.0.0.1:5500/FrontTerm/index.html
