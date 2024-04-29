e736c7f3c5e351744c904ca6901ff55288b955ae
Saul Sanchez <sanchezs@moravian.edu>
2024-04-28 22:33:38 -0400

    modified script to create 5500 server, run springboot and open html

--- StartApp.sh
+++ StartApp.sh
@@ -2,5 +2,19 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR/FrontTerm/termproject/"
 chmod +x "$SCRIPT_DIR/FrontTerm/termproject/gradlew"
+
+# Kill any processes using port 8080
+PID=$(lsof -t -i:8080)
+if [ -n "$PID" ]; then
+    kill -9 $PID
+    echo "Stopped running Spring Boot application"
+fi
+
 "$SCRIPT_DIR/FrontTerm/termproject/gradlew" bootRun &
-open -a "Google Chrome" http://127.0.0.1:5500/FrontTerm/index.html
+
+cd "$SCRIPT_DIR/FrontTerm/"
+http-server -p 5500 &
+
+sleep 5
+
+open -a "Google Chrome" http://127.0.0.1:5500/index.html
