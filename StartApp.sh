14b18245c1556dacd8f68475fcdda5c8085303e5
Saul Sanchez <sanchezs@moravian.edu>
2024-04-28 20:12:00 -0400

    added app zip file again

--- StartApp.sh
+++ StartApp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR/FrontTerm/termproject/"
+chmod +x FrontTerm/termproject/gradlew
 ./gradlew bootRun &
 open -a "Google Chrome" http://127.0.0.1:5500/FrontTerm/index.html
